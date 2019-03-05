f86d6f39fd710628d5b26c634c599ab37a8204f1
rc0032507 <rc0032507@CRO-24-06>
2019-03-05 10:43:44 +0000

    Add project files.

---
+++ areas/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace areas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double length = double.Parse(textBox1.Text);
+            double width = double.Parse(textBox2.Text);
+            double area = length * width;
+            textBox3.AppendText(area.ToString());
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
